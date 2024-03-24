621bc1d2c8e4fedeafb17adc1c00946f0a3ace9e
End-rey <andreyis2017@gmail.com>
2024-03-24 18:21:09 +0300

    fix issues

--- src/main/java/org/endrey/telephone/operator/dataLoader/DataLoader.java
+++ src/main/java/org/endrey/telephone/operator/dataLoader/DataLoader.java
@@ -1,7 +1,7 @@
 package org.endrey.telephone.operator.dataLoader;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,18 +9,26 @@ import org.endrey.telephone.operator.entity.Abonent;
 import org.endrey.telephone.operator.repository.AbonentRepositoryH2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 
 @Component
 public class DataLoader {
+
+    private final ResourceLoader resourceLoader;
     
     @Value("${file.path.abonents}")
     private String filePathAbonents;
 
     @Autowired
     private AbonentRepositoryH2 abonentRepository;
+
+    public DataLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     @PostConstruct
     public void loadAbonents() {
@@ -31,7 +39,8 @@ public class DataLoader {
 
     private Iterable<Abonent> readPhoneNumbersFromFile(String filePath) {
         List<Abonent> phoneNumbers = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        Resource resource = resourceLoader.getResource("classpath:" + filePath);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 phoneNumbers.add(new Abonent(line.trim()));
